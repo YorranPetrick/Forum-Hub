3228431ed19c1a32d14a66bd5e921c61a4edc065
Yorran Petrick <yorranpetrick27@gmail.com>
2025-08-14 19:31:11 -0300

    Realizando a criação da Classe TokenConfiguration

---
+++ src/main/java/com/yorranpetrick/forumhub/configuration/security/TokenConfiguration.java
@@ -0,0 +1,38 @@
+package com.yorranpetrick.forumhub.configuration.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.yorranpetrick.forumhub.models.autor.Autor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+
+@Service
+public class TokenConfiguration {
+
+
+    public String gerarToken(Autor autor) {
+        try {
+            var algoritmo = Algorithm.HMAC256("secret");
+            return JWT.create()
+                    .withIssuer("ForumHub")
+                    .withSubject(autor.getNome())
+                    .withClaim("senha", autor.getSenha())
+                    .withClaim("id", autor.getIdAutor())
+                    .withExpiresAt(dataExpiracao()) // Método para definir a data de expiração do token
+                    .sign(algoritmo);
+
+
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("erro ao gerar token jwt : " + e);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
